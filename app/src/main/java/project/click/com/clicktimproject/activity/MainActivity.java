f8904f1b7ed73988933b9e17bd372049ac145c01
Abolfazl 7x <aboolfazl46@gmail.com>
2018-09-06 14:57:56 +0430

    v1.2

--- app/src/main/java/project/click/com/clicktimproject/activity/MainActivity.java
+++ app/src/main/java/project/click/com/clicktimproject/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package project.click.com.clicktimproject;
+package project.click.com.clicktimproject.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import project.click.com.clicktimproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
